cbc31cd306a7c4d469c84ad299c5ea7ed4c758a0
Shrutideshpande2163 <146514326+Shrutideshpande2163@users.noreply.github.com>
2024-07-26 19:59:13 +0530

    Add files via upload

---
+++ init/index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
+main()
+    .then(() => {
+        console.log("Connection to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect(mongo_url);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "663cb6da47bf6fba03bb610e" }));
+    await Listing.insertMany(initData.data);
+    console.log("Data was initializd");
+};
+initDB();
